ead22200eb1e44652d2d3f82601a4b99ae9d8f77
lucianolima1933 <49759507+lucianolima1933@users.noreply.github.com>
2019-08-01 15:13:24 -0300

    Update bitmex_websocket_v1.1.py

--- bitmex_websocket_v1.1.py
+++ bitmex_websocket_v1.1.py
@@ -1,3 +1,16 @@
+#    DISCLAIMER:
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # bitmex_websocket_v1.1.py
 # an evolution of bitmex_data2py script
 # it works with BitMEX Websocket to display data, ccxt, curses and dialog libraries
@@ -597,3 +610,17 @@ curses.wrapper(main)
 
 # Finishing the code/program according to best practices:
 sys.exit()
+#
+#    DISCLAIMER:
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
